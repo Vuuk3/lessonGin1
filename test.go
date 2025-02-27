8056eaeccd0011676ad40b8ea37bd8df5740a1b3
Vuuk3 <petrovd1024@gmail.com>
2025-02-27 18:18:49 +0300

    second commit

--- test.go
+++ test.go
@@ -4,8 +4,24 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/dgrijalva/jwt-go"
+
+	"time"
 )
 
+
+var jwtKey = []byte("my_secret_key")
+
+type User struct {
+	Name string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 type Meals struct {
 	Name string `json:"name"`
@@ -17,6 +33,58 @@ var meals = []Meals{
 	{Name: "borscht", Cost: 300.5, Weight: 250},
 	{Name: "tea", Cost: 200, Weight: 500},
 }
+
+var users = []User{
+	{Name: "Иван", Password: "Ivan_1980"},
+}
+
+func generateToken(username string) (string, error) {
+	expirationTime := time.Now().Add(30 * time.Second)
+	claim := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	return token.SignedString(jwtKey)
+}
+
+func login(c *gin.Context) {
+	for _, user := range users {
+		err := c.BindJSON(&user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
+			return
+		}
+		if user.Name == "Иван" && user.Password == "Ivan_1980" {
+			token, err := generateToken(user.Name)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "could not create token"})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"token": token})
+			return
+		}
+	}
+	c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+}
+
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		claims := &Claims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(toke *jwt.Token) (interface{}, error){
+			return jwtKey, nil})
+		if !token.Valid || err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
 
 func getMeals(c *gin.Context) {
 	c.JSON(http.StatusOK, meals)
@@ -35,7 +103,12 @@ func getMealsByName(c *gin.Context) {
 
 func main() {
   r := gin.Default()
-  r.GET("/meals", getMeals)
-  r.GET("/meals/:name", getMealsByName)
+  r.POST("/login", login)
+  protected := r.Group("/")
+  protected.Use(authMiddleware())
+  {
+	protected.GET("/meals", getMeals)
+  	protected.GET("/meals/:name", getMealsByName)
+  }
   r.Run()
 }
